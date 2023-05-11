c40939c8e4047177106ee5aff67ae30fc341f4e2
Maik Kingma <maik.kingma@codecentric.nl>
2023-05-11 12:59:22 +0100

    removed unused import

--- src/main/java/uk/devoxx/tackle_eventual_consistency/acl/publisher/PublisherAppServiceImpl.java
+++ src/main/java/uk/devoxx/tackle_eventual_consistency/acl/publisher/PublisherAppServiceImpl.java
@@ -1,22 +1,20 @@
 package uk.devoxx.tackle_eventual_consistency.acl.publisher;
 
 import io.micrometer.common.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import uk.devoxx.tackle_eventual_consistency.domaininteraction.publisher.PublisherAppService;
-import uk.devoxx.tackle_eventual_consistency.domaininteraction.publisher.PublisherDTO;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import uk.devoxx.tackle_eventual_consistency.domaininteraction.publisher.PublisherAppService;
+import uk.devoxx.tackle_eventual_consistency.domaininteraction.publisher.PublisherDTO;
 
 import javax.json.Json;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * @author Maik Kingma
@@ -40,8 +38,8 @@ public class PublisherAppServiceImpl implements PublisherAppService {
         var result = restTemplate.getForObject(uri, PublisherPayload[].class);
 
         return Arrays.stream(Objects.requireNonNull(result))
-                .map(publisherPayload -> new PublisherDTO(publisherPayload.id, publisherPayload.name))
-                .toList();
+                       .map(publisherPayload -> new PublisherDTO(publisherPayload.id, publisherPayload.name))
+                       .toList();
     }
 
     @Override
@@ -63,10 +61,10 @@ public class PublisherAppServiceImpl implements PublisherAppService {
         var headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         var payload = Json.createObjectBuilder()
-                .add("publisherId", publisherId.toString())
-                .add("author", fullName)
-                .add("title", title)
-                .build().toString();
+                              .add("publisherId", publisherId.toString())
+                              .add("author", fullName)
+                              .add("title", title)
+                              .build().toString();
         var request = new HttpEntity<>(payload, headers);
         var publishingResponseDTO = restTemplate.postForObject(uri, request,
                 RequestPublishingResponsePayload.class);
@@ -83,7 +81,9 @@ public class PublisherAppServiceImpl implements PublisherAppService {
         return publisherServiceBaseUri + subPath;
     }
 
-    record PublisherPayload(UUID id, String name) { }
+    record PublisherPayload(UUID id, String name) {
+    }
 
-    record RequestPublishingResponsePayload(String isbn) { }
+    record RequestPublishingResponsePayload(String isbn) {
+    }
 }
